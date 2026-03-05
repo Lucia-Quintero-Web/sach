69181a451a3766956f34e19af032b2ba6eda0c40
Lucia-Quintero-Web <luciaquinterosach@gmail.com>
2026-03-05 15:25:59 -0500

    SEGUNDA VERSION

---
+++ js/config.js
@@ -0,0 +1,7 @@
+const SUPABASE_CONFIG = {
+    URL: "https://uvzmnrahykwbamsgtmek.supabase.co",
+    ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2em1ucmFoeWt3YmFtc2d0bWVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMjY2NzYsImV4cCI6MjA4NzcwMjY3Nn0.Omb-mQnFdw1931gpJVnz6IfkkxAZvSP786BC6Up9VjU",
+    PUBLISHABLE_KEY: "sb_publishable_3SBPYTvGuqcZKMGiHCIwew_XTYa5W9n"
+};
+
+export default SUPABASE_CONFIG;
